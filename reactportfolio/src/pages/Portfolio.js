d5a2f3f93689bde53add3acb9993adcc1c0639cc
Brandon Gart <brandongart@gmail.com>
2020-08-16 15:08:24 -0600

    Displays project to page

--- reactportfolio/src/pages/Portfolio.js
+++ reactportfolio/src/pages/Portfolio.js
@@ -3,12 +3,42 @@ import React from "react";
 class Portfolio extends React.Component {
 	constructor (props) {
 		super (props)
+		this.state = {
+			projects: [
+				{
+					name: "Employee Directory",
+					repo: "https://github.com/bgartdu/employeedirectory",
+					site: "",
+					image: "",
+
+				},
+				{
+					name: "Burger App",
+					repo: "https://github.com/bgartdu/burger",
+					site: "https://calm-brushlands-76584.herokuapp.com/",
+					image: "",
+					
+				},
+				{
+					name: "Note Taker",
+					repo: "https://github.com/bgartdu/notetaker1",
+					site: "https://tranquil-peak-25137.herokuapp.com/",
+					image: "",
+					
+				},
+
+
+			]
+		}
 	}
 	render(){
 		return <div>
-			<h1>
-				Portfolio
-			</h1>
+			<h2>Portfolio</h2>
+			<h3>Github: <a href= "https://github.com/bgartdu">bgartdu</a></h3>
+			{this.state.projects.map(item => (
+				<div>{item.name}</div>
+			))}
+
 		</div>
 	}
 }
