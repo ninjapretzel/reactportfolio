eae17ba7d9f6ab3fef0d563328fa34f777a49c71
ninjapretzel <ninjapretzel@yahoo.com>
2020-09-12 13:37:16 -0600

    Updates

---
+++ precache-manifest.d6939403474fe240bf37bd82f826ebf5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "564206d9e9e33ac5913111431319aab2",
+    "url": "/reactportfolio/index.html"
+  },
+  {
+    "revision": "1df08785dbff73b18c08",
+    "url": "/reactportfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb81aed84089ade98929",
+    "url": "/reactportfolio/static/js/2.8f02056b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactportfolio/static/js/2.8f02056b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1df08785dbff73b18c08",
+    "url": "/reactportfolio/static/js/main.6a60bfb2.chunk.js"
+  },
+  {
+    "revision": "ccc63777df2cb755b429",
+    "url": "/reactportfolio/static/js/runtime-main.ff3c2836.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactportfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
